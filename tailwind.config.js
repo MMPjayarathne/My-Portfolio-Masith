a684dcb227c031b3aec0081e12252b5d40534da7
MMPjayarathne <pramudithajayarathna@gmail.com>
2025-02-28 22:59:44 +0530

    create Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      colors: {
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        accent: "hsl(var(--accent))",
+        background: "hsl(var(--background))",
+        muted: "hsl(var(--muted))",
+      },
+    },
+  },
+  plugins: [],
+};
