fb16f5c9edd299d48fb899401ce24f2fa39c6d25
MMPjayarathne <pramudithajayarathna@gmail.com>
2025-03-01 23:37:44 +0530

    Update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import Navbar from "../components/Navbar";
 import Intro from "../components/Intro";
 import AboutMe from "@/components/Aboutme";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+import ContactMe from "@/components/Contactme";
+import Blogs from "@/components/Blogs";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Volunteer from "@/components/Volunteer";
 
 export default function Home() {
   return (
@@ -9,7 +15,14 @@ export default function Home() {
       <Navbar/>
       <Intro/>
       <AboutMe/>
+      <Experience/>
+      <Volunteer/>
       <Skills/>
+      <Projects/>
+      <ContactMe/>
+      <Blogs/>
+      <Footer/>
+
     </div>
   );
 }
